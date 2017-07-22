22886c7369b213154d5342e32b7323c07bd254d0
Dhan-Rheb Belza <dhanrheb@gmail.com>
2017-07-23 02:59:11 +0800

    Move all decorators to decorators.py

--- src/mmda/api/v1.py
+++ src/mmda/api/v1.py
@@ -1,56 +1,13 @@
-from functools import wraps
-
 from flask import Blueprint, g, jsonify
-import requests
 
-from mmda import config
-from mmda.repository import Feed
 from mmda.exceptions import ResourceNotFound
+from mmda.decorators import (
+    load_mmda_api, validate_highway,
+    validate_segment, validate_direction,
+)
 
 
 blueprint = Blueprint('v1', __name__)
-
-
-def load_mmda_api(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        response = requests.get(config.MMDA_API_URL)
-        g.feed = Feed(response.content)
-        return func(*args, **kwargs)
-    return wrapper
-
-
-def validate_highway(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        highway_id = kwargs.get('highway_id')
-        g.highway = g.feed.get_highway(highway_id)
-        if not g.highway:
-            raise ResourceNotFound('Highway')
-        return func(*args, **kwargs)
-    return wrapper
-
-
-def validate_segment(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        segment_id = kwargs.get('segment_id')
-        g.segment = g.feed.get_segment(segment_id)
-        if not g.segment:
-            raise ResourceNotFound('Segment')
-        return func(*args, **kwargs)
-    return wrapper
-
-
-def validate_direction(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        directions = ['NB', 'SB']
-        g.direction = kwargs.get('direction').upper()
-        if g.direction not in directions:
-            raise ResourceNotFound('Direction')
-        return func(*args, **kwargs)
-    return wrapper
 
 
 @blueprint.errorhandler(ResourceNotFound)
---
+++ src/mmda/decorators.py
@@ -0,0 +1,50 @@
+from functools import wraps
+
+from flask import g
+import requests
+
+from mmda import config
+from mmda.exceptions import ResourceNotFound
+from mmda.repository import Feed
+
+
+def load_mmda_api(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        response = requests.get(config.MMDA_API_URL)
+        g.feed = Feed(response.content)
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def validate_highway(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        highway_id = kwargs.get('highway_id')
+        g.highway = g.feed.get_highway(highway_id)
+        if not g.highway:
+            raise ResourceNotFound('Highway')
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def validate_segment(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        segment_id = kwargs.get('segment_id')
+        g.segment = g.feed.get_segment(segment_id)
+        if not g.segment:
+            raise ResourceNotFound('Segment')
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def validate_direction(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        directions = ['NB', 'SB']
+        g.direction = kwargs.get('direction').upper()
+        if g.direction not in directions:
+            raise ResourceNotFound('Direction')
+        return func(*args, **kwargs)
+    return wrapper
