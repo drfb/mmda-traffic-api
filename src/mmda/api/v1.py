6e9f718947d19a4cb13c1dc85d93e96760c98b55
Dhan-Rheb Belza <dhan@cocoonlabs.io>
2017-07-22 19:02:32 +0800

    Add mmda rss parser

--- src/mmda/api/v1.py
+++ src/mmda/api/v1.py
@@ -1,7 +1,25 @@
-from flask import Blueprint, jsonify
+from functools import wraps
+
+from flask import Blueprint, g, jsonify
+import requests
+
+from mmda import config
+from mmda.utils import parse_mmda_rss
 
 
 blueprint = Blueprint('v1', __name__)
+
+
+def load_mmda_api(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        response = requests.get(config.MMDA_API_URL)
+        g.mmda = {
+            'status_code': response.status_code,
+            'data': parse_mmda_rss(response.content)
+        }
+        return func(*args, **kwargs)
+    return wrapper
 
 
 @blueprint.route('/')
